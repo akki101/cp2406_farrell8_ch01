1f00b0b850d8a096adcda414dc45681e6fced787
Akki <ankitjakhar4@gmail.com>
2017-08-13 21:29:22 +1000

    Initial commit

---
+++ PracExercises/src/TableAndChairs.java
@@ -0,0 +1,11 @@
+public class TableAndChairs {
+    public static void main(String []arg){
+        System.out.println("x                      x");
+        System.out.println("x                      x");
+        System.out.println("x     xxxxxxxxxxxx     x");
+        System.out.println("xxxxx x          x xxxxx");
+        System.out.println("x   x x          x x   x");
+        System.out.println("x   x x          x x   x");
+
+    }
+}
