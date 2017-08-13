fe2b9b8243fbaaf6ca7f80f8ca38df729c18c934
Akki <ankitjakhar4@gmail.com>
2017-08-13 19:08:25 +1000

    bug fixed

--- CodeInFigures/FirstDialog.java
+++ CodeInFigures/FirstDialog.java
@@ -3,6 +3,7 @@ public class FirstDialog
 {
    public static void main(String[] args)
    {
+
       JOptionPane.showMessageDialog(null, "First Java dialog");
    }
 }
--- DebuggingExercises/FixDebugOne2.java
+++ DebuggingExercises/FixDebugOne2.java
@@ -1,8 +1,6 @@
-public class FixDebugOne2
-{
+public class FixDebugOne2{
    // This program displays some output
-   public static void main(String args)
-   {
+   public static void main(String [] args){
       System.out.println("Java programming is fun.");
       System.out.println("Getting a program to work");
       System.out.println("can be a challenge,");
