6286d9dacc789f980610b7c1089fa583def4e3fa
Akki <ankitjakhar4@gmail.com>
2017-08-13 21:49:13 +1000

    Initial commit

---
+++ PracExercises/src/BurmaShave.java
@@ -0,0 +1,21 @@
+import javax.swing.JOptionPane;
+public class BurmaShave {
+    public static void main(String []arg){
+        JOptionPane.showMessageDialog(null,"The place to pass");
+        JOptionPane.showMessageDialog(null,"On curves");
+        JOptionPane.showMessageDialog(null,"You know");
+        JOptionPane.showMessageDialog(null,"Is only at");
+        JOptionPane.showMessageDialog(null,"A beauty show");
+    }
+}
+
+
+
+
+
+
+/*
+On curves
+You know
+Is only at
+A beauty show*/
---
+++ PracExercises/src/Comments.java
@@ -0,0 +1,8 @@
+public class Comments {
+/*Program comments are non executing statements you add to a file for the purpose of documentation.*/
+/**Program comments are non executing statements you add to a file for the purpose of documentation.*/
+    public static void main(String []arg){
+        //Program comments are non executing statements you add to a file for the purpose of documentation.
+        System.out.println("Program comments are non executing statements you add to a file for the purpose of documentation.");
+    }
+}
