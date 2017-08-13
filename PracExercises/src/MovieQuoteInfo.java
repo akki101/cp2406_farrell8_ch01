8b66831c7cb9beddffb7d3b35815e714fe873a94
Akki <ankitjakhar4@gmail.com>
2017-08-13 20:58:48 +1000

    Initial commit

---
+++ PracExercises/src/MovieQuoteInfo.java
@@ -0,0 +1,6 @@
+public class MovieQuoteInfo {
+    public static void main(String []arg){
+        System.out.println("Movie Quote details :");
+        System.out.println("Do, or do not.There is no \"try\".\t By Yoda \t From - Star Wars   1980");
+    }
+}
---
+++ PracExercises/src/MovieQuotes.java
@@ -0,0 +1,5 @@
+public class MovieQuotes {
+    public static void main(String [] arg){
+        System.out.println("Movie Quote:\nLife's too short to miss out anything, try to take it slowly.");
+    }
+}
